e4cbc28cb6cc2aeab3ebca9fc38d904d4731ae24
Krzysztof <zamienny06@gmail.com>
2025-02-21 13:28:55 +0100

    Added labels in polish

--- Converter/forms.py
+++ Converter/forms.py
@@ -3,38 +3,46 @@ from django import forms
 
 class CalcLengthForm(forms.Form):
     units = [
-        
-        ('mm', 'milimetry'), 
-        ('cm', 'centymetry'), 
-        ('dm', 'decymetry'), 
+
+        ('mm', 'milimetry'),
+        ('cm', 'centymetry'),
+        ('dm', 'decymetry'),
         ('m', 'metry'),
         ('km', 'kilometry')
     ]
-    length = forms.FloatField(required=True)
-    unit_to_convert = forms.ChoiceField(choices=units, required=True, initial='m')
-    unit_from_convert = forms.ChoiceField(choices=units, required=True)
+    length = forms.FloatField(required=True, label="Długość: ")
+    unit_to_convert = forms.ChoiceField(
+        choices=units, required=True, initial='m', label="Przekonwertować Z: ")
+    unit_from_convert = forms.ChoiceField(
+        choices=units, required=True, label=" Przekonwertować NA: ")
+
 
 class CalcWeightForm(forms.Form):
     units = [
-        
-        ('mg', 'miligramy'), 
-        ('g', 'gramy'), 
-        ('dag', 'dekagramy'), 
+
+        ('mg', 'miligramy'),
+        ('g', 'gramy'),
+        ('dag', 'dekagramy'),
         ('kg', 'kilogramy'),
         ('t', 'tonay')
     ]
-    weight = forms.FloatField(required=True)
-    unit_to_convert = forms.ChoiceField(choices=units, required=True, initial='kg')
-    unit_from_convert = forms.ChoiceField(choices=units, required=True)
+    weight = forms.FloatField(required=True, label="Waga: ")
+    unit_to_convert = forms.ChoiceField(
+        choices=units, required=True, initial='kg', label="Przekonwertować Z: ")
+    unit_from_convert = forms.ChoiceField(
+        choices=units, required=True, label=" Przekonwertować NA: ")
+
 
 class CalcTempForm(forms.Form):
     units = [
-        
-        ('C', 'Celsjusza'), 
-        ('F', 'Fahrenheita '), 
-        ('K', 'Kelwina'), 
+
+        ('C', 'Celsjusza'),
+        ('F', 'Fahrenheita '),
+        ('K', 'Kelwina'),
         ('R', 'Rankinea'),
     ]
-    temperature = forms.FloatField(required=True)
-    unit_to_convert = forms.ChoiceField(choices=units, required=True, initial='F')
-    unit_from_convert = forms.ChoiceField(choices=units, required=True)
+    temperature = forms.FloatField(required=True, label="Temperatura:")
+    unit_to_convert = forms.ChoiceField(
+        choices=units, required=True, initial='F', label="Przekonwertować Z: ")
+    unit_from_convert = forms.ChoiceField(
+        choices=units, required=True, label="Przekonwertować NA: ")
